189825d55c72a1997efa537ba1df26b557ab5b5f
Michael McShinsky <michael@utahesports.net>
2018-08-07 12:11:52 -0600

    url update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "August Lan Party",
+        "excerpt":"Bring out your rigs!Bring out your consoles!But most of all bring your beautiful self! —POT LUCK—Bring your favorite dish to share with others! Cost of admission:(Event) BYOC = $10(Event) = $15(Hourly) = $3/hr ***This includes the hours from 8PM - 8AM. Why should you come?Computers are available on-site for use.Gigabit...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-08-03-August-Lan-Party",
+        "teaser":null},{
+        "title": "Fortnite 2v2 $850 prize money tournament (PS4)",
+        "excerpt":"Fortnite 2v2 TournamentLayton Hills Mall10amAugust 7thCost is $50.00 per Team GB ENT gaming center Located Lower level near Dillards MUST BE AT THE MALL TO PLAY IN TOURNAMENT Best of 3 Matches: The team with the most kills at the end of the game session will be awarded the win...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-08-07-Fortnite-2v2",
+        "teaser":null},{
+        "title": "Super Street Fighter IV Arcade Edition",
+        "excerpt":"Join us at the Microsoft Store in Fashion Place Mall on August 11th from 6p-8p for a Street Fighter IV Tournament. We will have Xbox swag for participants and a prize for the tournament winner. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-08-11-Super-Street-Fighter-IV-Arcade-Edition",
+        "teaser":null},{
+        "title": "GameTyrant Melee Monthly",
+        "excerpt":"GameTyrant’s Alex Refresh are excited to host another Melee Touranment. PLEASE BRING SET UPS!!! If you bring a full set up I can give you 5$ off venue. Capped at 25 set ups. This tournament is for the player first and foremost! 100% transparency is our gameplan so please if...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-08-18-GameTyrant-Melee-Monthly",
+        "teaser":null},{
+        "title": "GameTyrant Melee Monthly",
+        "excerpt":"Online Event. See details event link for more information. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-08-19-Halo-5-Online-2v2-Tournament",
+        "teaser":null},{
+        "title": "The Salt Packet",
+        "excerpt":"The Salt Packet is bringing to the table what many of our Utahn players love– Money Matches. That’s right, Rivals of Aether players and Super Smash Bros. for WiiU players will have hours upon hours of plenty of money matches. Ever been salty at someone’s Cloud after a terrible bracket...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-08-25-The-Salt-Packet",
+        "teaser":null},{
+        "title": "GameTyrant 1v1 PC Fortnite Tournament ",
+        "excerpt":"Doors open at NOON! $15 Entry. $5 of entry goes to the Prize PotDouble Elimination Head to head event. Console players may also compete! (must bring own controller, and have an Epic account) BYOC for $5 off of the entry fee. THINGS YOU NEED: you must have an Epic account....","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-09-01-GameTyrant-1v1-PC-Fortnite-Tournament",
+        "teaser":null},{
+        "title": "FanX® Salt Lake Comic Convention 2018",
+        "excerpt":"FanX® Salt Lake Comic Convention™s (formerly Salt Lake Comic Con) are Dan Farr Productions’ annual events, and are now the most attended conventions in the state of Utah and the largest comic convention in North America per capita. The conventions have attracted celebrities like Stan Lee, Chris Evans, Mark Hamill,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-09-06-FanX-Salt-Lake-Comic-Convention",
+        "teaser":null},{
+        "title": "Get Lanlocked",
+        "excerpt":"Two of our favorite things are LAN parties, and vacations, and we’ve had a vision for a long time of combining the two. We were finally able to test it out, and weren’t surprised to find that everyone loved it. Not only does it create an entirely new experience by...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-09-06-Get-Lanlocked",
+        "teaser":null},{
+        "title": "GameTyrant Expo 2018",
+        "excerpt":"GameTyrant Expo is BACK After the Legendary set of events that transpired last year in both Melee and Smash 4, GT-X returns November 2nd-4th at the Gateway Mall in Downtown Salt Lake City, Utah! Featuring: Smash Melee (Singles, Doubles) Smash Wii U (Singles, Doubles) Smash 64 (Singles, Doubles) Dragonball FighterZ...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-11-02-GameTyrant-Expo",
+        "teaser":null},{
+        "title": "UNC Holiday Party",
+        "excerpt":"Join us for our the UtahNintendo Community November event on Saturday, December 8tth from 1:30 to 4:30 pm MST. RSVP now to get notifications when more details are posted. With Super Smash Bros. Ultimate releasing on Friday, December 7th, you can guarantee that we will be playing A LOT at...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-12-08-UNC-Holidy-Party",
+        "teaser":null},{
+        "title": "Round 1 Sandy Grand Opening!",
+        "excerpt":"We’ve received word that the grand opening for the South Town Round 1 arcade will be on Saturday the 11th of August! Come join us as we welcome Round 1 Arcade to Utah! Round 1 Arcade is a nationwide chain of arcades which feature the best games, including the newest...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/events/2018-08-11-Round-1-Sandy-Grand-Opening",
+        "teaser":null},]
